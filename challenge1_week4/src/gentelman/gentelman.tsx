bb6c3bb88b804c8ba05fa6b861f1d7c88dd9c2d4
Alejandro de Oyarbide <oyarbide_de@hotmail.com>
2023-08-08 01:30:48 +0200

    add gentlemans and text header

--- challenge1_week4/src/gentelman/gentelman.tsx
+++ challenge1_week4/src/gentelman/gentelman.tsx
@@ -1,52 +1,20 @@
-const casposos= [
-  {
-    id: 1,
-    name: 'Bertin Osborne',
-    status: 'Alive',
-    profession: 'Youtuber',
-    twitter: '@osbourne',
-    picture: 'bertin.jpg',
-    alternativeText: 'Osbourne pointing at you',
-    selected: true,
-  },
-  {
-    name: 'The Farytale',
-    status: 'RIP',
-    profession: 'Influencer',
-    twitter: 'pending',
-    picture: 'fary.jpg',
-    alternativeText: 'The Fary pointing at you',
-    id: 2,
-    selected: false,
-  },
-  {
-    id: 3,
-    name: 'Julius Churchs',
-    status: 'Alive',
-    profession: 'Java developer',
-    twitter: '@julius_churchs',
-    picture: 'julio.jpg',
-    alternativeText: 'Churchs pointing at you',
-    selected: true,
-  },
-];
+import { sexyPeople } from '../data/reallySexyPeople';
 
-const casposos =()=>{
+export function Main() {
   return (
-    
-    <section className="controls">
-      <p className="info">0 gentlemen pointing at you</p>
-      <button className="button button--select">Select all</button>
-    </section>
-    <main className="main">
-      <ul className="gentlemen">
+    <ul className="gentleman">
+      {sexyPeople.map((item) => (
         <li className="gentleman">
           <div className="gentleman__avatar-container">
-            <img className="gentleman__avatar" src="./imagenes/fary.jpg" alt="The Fary pointing at you" />
+            <img
+              className="gentleman__avatar"
+              src={item.picture}
+              alt="The Fary pointing at you"
+            />
             <span className="gentleman__initial">F</span>
           </div>
           <div className="gentleman__data-container">
-            <h2 class="gentleman__name">The Fary</h2>
+            <h2 className="gentleman__name">The Fary</h2>
             <ul className="gentleman__data-list">
               <li className="gentleman__data">
                 <span className="gentleman__data-label">Profession:</span>
@@ -63,7 +31,7 @@ const casposos =()=>{
           <i className="icon gentleman__icon fas fa-check"></i>
           <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
         </li>
-      </ul>
-    </main>
+      ))}
+    </ul>
   );
 }
--- challenge1_week4/src/info/app/App.tsx
+++ challenge1_week4/src/info/app/App.tsx
@@ -1,4 +1,5 @@
 import { Buttons } from '../../buton/button';
+import { Main } from '../../gentelman/gentelman';
 import { Header } from '../info';
 
 export function App() {
@@ -6,6 +7,7 @@ export function App() {
     <>
       <Header></Header>
       <Buttons></Buttons>
+      <Main></Main>
     </>
   );
 }
